2a39f636df7e074e30f2fa872a1804bf862e0ca4
Andrew <an.carev4@gmail.com>
2022-12-03 19:58:51 +0300

    feat: implement solution for `zlib` module tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream, access } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const fileToCompressPath = path.join(_dirname, "files", "fileToCompress.txt");
+const archiveFilePath = path.join(_dirname, "files", "archive.gz");
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = createGzip();
+    const source = createReadStream(fileToCompressPath);
+    const destination = createWriteStream(archiveFilePath);
+
+    await pipe(source, gzip, destination);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream, access } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const fileToDecompressPath = path.join(_dirname, "files", "archive.gz");
+const decompressedFilePath = path.join(_dirname, "files", "fileToCompress.txt");
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const unzip = createUnzip();
+    const source = createReadStream(fileToDecompressPath);
+    const destination = createWriteStream(decompressedFilePath);
+
+    await pipe(source, unzip, destination);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 };
 
 await decompress();
