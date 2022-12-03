634c18c4e6e02211fea082cab61d9c5c4f2c2611
Andrew <an.carev4@gmail.com>
2022-12-03 16:47:18 +0300

    feat: implement solution for `cli` module tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import { env } from "process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const variablesWithPrefix = Object.entries(env).filter(([key, value]) =>
+    key.startsWith("RSS_")
+  );
+  const output = variablesWithPrefix
+    .map(([variableName, variableValue]) => `${variableName}=${variableValue}`)
+    .join("; ");
+
+  console.log(output);
 };
 
 parseEnv();
