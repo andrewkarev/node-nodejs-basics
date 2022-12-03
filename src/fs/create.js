d58de3ce0acdbfabc80b6f6de801780166d52df0
Andrew <an.carev4@gmail.com>
2022-12-03 16:41:01 +0300

    feat: implement solution for `fs` module tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const destination = path.join(_dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.appendFile(destination, "I am fresh and young", {
+      flag: "ax",
+    });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
-const rename = async () => {
-    // Write your code here 
+import * as fs from "node:fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const initialFileName = path.join(_dirname, "files", "wrongFilename.txt");
+const newFileName = path.join(_dirname, "files", "properFilename.md");
+
+const rename = () => {
+  fs.access(newFileName, (error) => {
+    if (!error) throw new Error("FS operation failed");
+
+    fs.rename(initialFileName, newFileName, (error) => {
+      if (error) throw new Error("FS operation failed");
+    });
+  });
 };
 
-await rename();
+rename();
