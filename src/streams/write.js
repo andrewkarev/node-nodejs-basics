e369a5bbde5040978b2b5ee3025279f5a16904c6
Andrew <an.carev4@gmail.com>
2022-12-03 18:47:33 +0300

    feat: implement solution for `streams` module task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+import { stdin as input, stdout as output } from "process";
+import readline from "node:readline";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const fileToWritePath = path.join(_dirname, "files", "fileToWrite.txt");
+const prompt = "Type something to see the output:\n";
+const goodbyeMessage =
+  '\nBye! The entered data is saved in a "fileToWrite.txt".\n';
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(fileToWritePath);
+  const rl = readline.createInterface({ input, output, prompt });
+
+  rl.prompt();
+  rl.on("line", (input) => writableStream.write(`${input}\n`));
+  rl.on("close", () => output._write(goodbyeMessage));
 };
 
 await write();
