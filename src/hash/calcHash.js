c3c9e3666db2b99747d9844e57233b7ff324f9d4
Andrew <an.carev4@gmail.com>
2022-12-03 16:48:43 +0300

    feat: implement solution for `hash` module task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from "node:crypto";
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const fileToHashPath = path.join(
+  _dirname,
+  "files",
+  "fileToCalculateHashFor.txt"
+);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const content = await fs.readFile(fileToHashPath, "utf-8");
+  const hash = createHash("sha256").update(content).digest("hex");
+  console.log(hash);
 };
 
 await calculateHash();
